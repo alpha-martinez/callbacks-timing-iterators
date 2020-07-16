7ed118a3f4d51341dc66440e29554e9a30b441cd
Alpha Martinez <alphamartinez@Alphas-MacBook-Pro.local>
2020-07-16 18:06:51 -0400

    greeting

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-function greeting(name) {
+function greeting(name, callback) {
     let sentence = 'Hello, my name is ' + name;
 
+    let otherSentence = callback();
+
+    return sentence + otherSentence;
+}
+
+
+
+
+function anotherGreeting() {
+    let sentence = 'I forgot my last name ';
+    
+    
     return sentence;
 }
 
+
 let myName = 'Rome';
-console.log(greeting(myName));
+console.log(greeting(myName, anotherGreeting));
